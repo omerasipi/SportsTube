c1f277c7113248f287a318603bb41c4257db8643
omerasipi <omer.asipi@stud.kftg.ch>
2018-12-13 11:14:59 +0100

    new

--- index.php
+++ index.php
@@ -1,199 +1,127 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SportClips</title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/custom.css" >
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<nav class="navbar navbar-dark bg-dark justify-content-between">
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1>SportsTube</h1>
+        <p>Clips, Tips & Aims</p>
+    </div>
+</div>
 
-    <a class="navbar-brand" style="color: white;">SportsTube</a>
-
-    <form class="form-inline ">
-        <input class="form-control mr-sm-2" type="text" placeholder="Benutzername" aria-label="Benutzername">
-        <input class="form-control mr-sm-2" type="password" placeholder="Passwort" aria-label="Passwort">
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Login</button>
-    </form>
-
-
-<!--    <form class="form-inline">-->
-<!--        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">-->
-<!--        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>-->
-<!--    </form>-->
-
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://www.kanti-frauenfeld.ch/">KFTG</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Clips</a></li>
+                <li><a href="edit.php">Verwaltung</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Dein Account</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
 
 <div class="container">
-    <div class="input-group mb-3" style="margin-top: 2em">
-        <input type="text" class="form-control suche" placeholder="Suchen" aria-label="Suchen" aria-describedby="basic-addon2">
-        <div class="input-group-append">
-            <button class="btn btn-outline-secondary  suche" type="button">Suchen</button>
-        </div>
-    </div>
+    <form class="form-inline">
+        <input type="email" class="form-control" size="50" placeholder="Tags">
+        <button type="button" class="btn btn-primary">Suchen</button>
+    </form>
 </div>
-
-<!--<div class="container-fluid">-->
-<!--    <div class="row align-items-center">-->
-<!--        <div class="col-sn">-->
-<!--            <img src="img/bild1.jpg" alt="..." class="img-thumbnail">-->
-<!--        </div>-->
-<!--        <div class="col-sn">-->
-<!--            <img src="img/m3.jpg" alt="..." class="img-thumbnail">-->
-<!--        </div>-->
-<!--        <div class="col-sn">-->
-<!--            <img src="img/bild1.jpg" alt="..." class="img-thumbnail">-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-
-
-
-<div class="container-fluid" style="padding: 60px">
+<br><br>
+<div class="container">
     <div class="row">
-        <div class="row">
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
-                   data-image="img/bild1.jpg"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="img/bild1.jpg"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
-                   data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-                <p class="thumbnail-text">Audi</p>
-            </div>
-
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title=""
-                   data-image="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Test1"
-                   data-image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-
-
-
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-
-
-
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
-            </div>
-            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-                <a class="thumbnail" href="#" data-image-id="" data-toggle="modal" data-title="Im so nice"
-                   data-image="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                   data-target="#image-gallery">
-                    <img class="img-thumbnail"
-                         src="https://images.pexels.com/photos/158971/pexels-photo-158971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                         alt="Another alt text">
-                </a>
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><video class="video-format" controls><source src="videos/Chestbounce.mov"></video></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
             </div>
         </div>
-
-
-<!--        <div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">-->
-<!--            <div class="modal-dialog modal-lg">-->
-<!--                <div class="modal-content">-->
-<!--                    <div class="modal-header">-->
-<!--                        <h4 class="modal-title" id="image-gallery-title"></h4>-->
-<!--                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span>-->
-<!--                        </button>-->
-<!--                    </div>-->
-<!--                    <div class="modal-body">-->
-<!--                        <img id="image-gallery-image" class="img-responsive col-md-12" src="">-->
-<!--                    </div>-->
-<!--                    <div class="modal-footer">-->
-<!--                        <button type="button" class="btn btn-secondary float-left" id="show-previous-image"><i class="fa fa-arrow-left"></i>-->
-<!--                        </button>-->
-<!---->
-<!--                        <button type="button" id="show-next-image" class="btn btn-secondary float-right"><i class="fa fa-arrow-right"></i>-->
-<!--                        </button>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><img src="https://placehold.it/150x80?text=VIDEO" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><img src="https://placehold.it/150x80?text=VIDEO" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><img src="https://placehold.it/150x80?text=VIDEO" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><img src="https://placehold.it/150x80?text=VIDEO" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">sportkrass.mp4</div>
+                <div class="panel-body"><img src="https://placehold.it/150x80?text=VIDEO" class="img-responsive" style="width:100%" alt="Image"></div>
+                <div class="panel-footer">
+                    <span class="badge badge-primary">Hochsprung</span>
+                    <span class="badge badge-primary">1M</span>
+                    <span class="badge badge-primary">Haha</span>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</div><br><br>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<footer class="container-fluid text-center">
+    <p>2018 © Patrik Studer & Omer Asipi</p>
+</footer>
+
 </body>
 </html>
